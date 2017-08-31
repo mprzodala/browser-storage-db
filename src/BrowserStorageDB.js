0cc45b05326eebe15d4e614aea09bbca2df544c4
Mariusz Przodała <mariusz.przodala@proserwit.pl>
2017-08-31 23:03:32 +0200

    rename

---
+++ src/BrowserStorageDB.js
@@ -0,0 +1,69 @@
+const generateDocumentId = () => Math.random().toString(36).substring(7);
+
+class BrowserStorageDB {
+    static isMatchQuery(document, query = {}) {
+        let isMatched = true;
+        Object.keys(query).forEach((key) => {
+            if (document[key] !== query[key]) isMatched = false;
+        });
+        return isMatched;
+    }
+
+    static filterByQuery(documents, query) {
+        const results = [];
+        documents.forEach((document) => {
+            if (BrowserStorageDB.isMatchQuery(document, query)) results.push(document);
+        });
+        return results;
+    }
+
+    constructor(name, storage) {
+        this.name = name;
+        this.storage = storage;
+    }
+
+    set(state) {
+        const data = JSON.stringify(state);
+        this.storage.setItem(this.name, data);
+    }
+
+    insert(document) {
+        const data = JSON.parse(this.storage.getItem(this.name) || JSON.stringify([]));
+        const documentId = generateDocumentId();
+        data.push(Object.assign({}, document, { id: documentId }));
+        this.set(data);
+        return documentId;
+    }
+
+    find(query) {
+        const documents = JSON.parse(this.storage.getItem(this.name) || JSON.stringify([]));
+        return BrowserStorageDB.filterByQuery(documents, query);
+    }
+
+    findOne(query) {
+        const [document] = this.find(query);
+        return document;
+    }
+
+    update(id, data) {
+        const documents = this.find();
+        const [document] = BrowserStorageDB.filterByQuery(documents, { id });
+        const documentIndex = documents.indexOf(document);
+        documents[documentIndex] = Object.assign({}, document, data);
+        this.set(documents);
+    }
+
+    remove(id) {
+        const documents = this.find();
+        const [document] = BrowserStorageDB.filterByQuery(documents, { id });
+        const documentIndex = documents.indexOf(document);
+        documents.splice(documentIndex, 1);
+        this.set(documents);
+    }
+
+    clear() {
+        this.storage.clear();
+    }
+}
+
+export default BrowserStorageDB;
